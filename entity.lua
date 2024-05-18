3eb8a3fb4372646501bcb5b3280da6da8eadb5ac
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-05-18 17:03:16 +0800

    Update entity.lua

--- entity.lua
+++ entity.lua
@@ -5,3 +5,16 @@ end
 function ENTMETA:AddEFlags(eflags)--Fix cant add EFL_KILLME
   self:SetSaveValue("m_iEFlags",bit.bor(self:GetEFlags(),eflags))
 end
+function ENTMETA:StopThink()
+  self:NextThink(CurTime()+1e9)
+  if(self.Think)then self.Think=function() end end
+end
+function ENTMETA:MarkAsRemoved()
+  self:AddEFlags(EFL_KILLME)
+end
+function ENTMETA:UnmarkAsRemoved()
+  self:RemoveEFlags(EFL_KILLME)
+end
+function ENTMETA:IsMarkedAsRemoved()
+  return self:IsEFlagSet(EFL_KILLME)
+end
