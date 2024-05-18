6a1244ecba563efff1e65c47bbbdeef88ca39ea0
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-05-18 16:52:26 +0800

    Update entity.lua
    comment

--- entity.lua
+++ entity.lua
@@ -2,6 +2,6 @@ local ENTMETA=FindMetaTable 'Entity'
 function ENTMETA:SetEFlags(eflags)
   self:SetSaveValue("m_iEFlags",eflags)
 end
-function ENTMETA:AddEFlags(eflags)
+function ENTMETA:AddEFlags(eflags)--Fix cant add EFL_KILLME
   self:SetSaveValue("m_iEFlags",bit.bor(self:GetEFlags(),eflags))
 end
