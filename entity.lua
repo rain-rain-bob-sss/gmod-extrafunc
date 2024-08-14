9b54bee64ddc273ba34feff5df486d038ead63b5
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-08-14 14:34:35 +0800

    Update entity.lua

--- entity.lua
+++ entity.lua
@@ -1,34 +1,80 @@
-local ENTMETA=FindMetaTable 'Entity'
+local ENTMETA = FindMetaTable"Entity"
+local SSV = ENTMETA.SetSaveValue
+local GSV = ENTMETA.GetSaveValue
+ENTMETA.SetEFlags_Old = ENTMETA.SetEFlags
+ENTMETA.addEFlags_Old = ENTMETA.AddEFlags
 function ENTMETA:SetEFlags(eflags)
-  self:SetSaveValue("m_iEFlags",eflags)
+    SSV(self, "m_iEFlags", eflags)
 end
-function ENTMETA:AddEFlags(eflags)--Fix cant add EFL_KILLME
-  self:SetSaveValue("m_iEFlags",bit.bor(self:GetEFlags(),eflags))
+
+function ENTMETA:AddEFlags(eflags) --Fix cant add EFL_KILLME
+    SSV(self, "m_iEFlags'", bit.bor(self:GetEFlags(), eflags))
 end
+
 function ENTMETA:StopThink()
-  self:NextThink(CurTime()+1e9)
-  if(self.Think)then self.Think=function() end end
+    self:NextThink(CurTime() + 1e9)
+    if self.Think then self.Think = function() end end
 end
+
 function ENTMETA:MarkAsRemoved()
-  self:AddEFlags(EFL_KILLME)
+    self:AddEFlags(EFL_KILLME)
 end
+
 function ENTMETA:UnmarkAsRemoved()
-  self:RemoveEFlags(EFL_KILLME)
+    self:RemoveEFlags(EFL_KILLME)
 end
+
 function ENTMETA:IsMarkedAsRemoved()
-  return self:IsEFlagSet(EFL_KILLME)
+    return self:IsEFlagSet(EFL_KILLME)
 end
-UTIL_Remove=ENTMETA.Remove
-util.Remove=ENTMETA.Remove
-local vec_zero=Vector(0,0,0)
+
+UTIL_Remove = ENTMETA.Remove
+util.Remove = ENTMETA.Remove
+local vec_zero = Vector(0, 0, 0)
 function ENTMETA:StopVelocity()
-  if(self:IsPlayer())then self:SetVelocity(-self:GetVelocity()) end
-  self:SetVelocity(vec_zero)
-  for i=0,self:GetPhysicsObjectCount() do
-    local phy=self:GetPhysicsObjectNum(i)
-    if(IsValid(phy))then
-        phy:SetVelocity(vec_zero)
+    if self:IsPlayer() then self:SetVelocity(-self:GetVelocity()) end
+    self:SetVelocity(vec_zero)
+    for i = 0, self:GetPhysicsObjectCount() do
+        local phy = self:GetPhysicsObjectNum(i)
+        if IsValid(phy) then phy:SetVelocity(vec_zero) end
     end
-  end
-  self:SetSaveValue("m_flVelocity",tostring(vec_zero))
+
+    SSV(self, "m_flVelocity", tostring(vec_zero))
+end
+
+function ENTMETA:SetThrower(thrower)
+    SSV(self, "m_hThrower", thrower)
+    if GSV(self, "m_hOriginalThrower") == NULL then SSV(self, "m_hOriginalThrower", thrower) end
+end
+
+function ENTMETA:GetThrower()
+    return GSV(self, "m_hThrower")
+end
+
+function ENTMETA:GetOriginalThrower()
+    return GSV(self, "m_hOriginalThrower")
+end
+
+function ENTMETA:GRENADE_SetLive(live)
+    return SSV(self, "m_bIsLive", true)
+end
+
+function ENTMETA:GRENADE_IsLive()
+    return GSV(self, "m_bIsLive")
+end
+
+function ENTMETA:GRENADE_GetDamageRadius()
+    return GSV(self, "m_DmgRadius")
+end
+
+function ENTMETA:GRENADE_SetDamageRadius(rad)
+    return SSV(self, "m_DmgRadius", rad)
+end
+
+function ENTMETA:GRENADE_GetDetonateTime()
+    return GSV(self, "m_flDetonateTime")
+end
+
+function ENTMETA:GRENADE_HasWarnedAI()
+    return GSV(self, "m_bHasWarnedAI")
 end
